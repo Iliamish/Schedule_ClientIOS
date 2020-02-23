38f1c8072c6b42ab00bc4d421c7a2d924cc4fa1e
Илья <Bond@iMac.local>
2020-02-23 22:57:40 +0300

    yes

---
+++ sсhedule4unn/shedule4unn/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  shedule4unn
+//
+//  Created by Илья on 23.02.2020.
+//  Copyright © 2020 Илья. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
