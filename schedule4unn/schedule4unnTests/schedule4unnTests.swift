fc289c155ef42be82dfd18315c95bb29927eac17
Илья <Bond@iMac.local>
2020-03-20 13:20:59 +0300

    create project

---
+++ schedule4unn/schedule4unnTests/schedule4unnTests.swift
@@ -0,0 +1,34 @@
+//
+//  schedule4unnTests.swift
+//  schedule4unnTests
+//
+//  Created by Илья on 20.03.2020.
+//  Copyright © 2020 iailil. All rights reserved.
+//
+
+import XCTest
+@testable import schedule4unn
+
+class schedule4unnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
